69f2732e61a237c40f66e2ebe195836ee6f25a14
Yury Butskevich <bxyura@gmail.com>
2024-03-26 23:24:59 +0300

    feat: complete getIdGeneratorFunction func

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -227,8 +227,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let newId = startFrom - 1;
+  const getId = () => {
+    newId += 1;
+    return newId;
+  };
+  return getId;
 }
 
 module.exports = {
